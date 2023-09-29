574f9ca1862193a38783f5828792ce9e2ad16a27
shreyashkatkar07 <shreyashkatkar04@gmail.com>
2023-09-29 15:25:34 +0530

    rendered newsitems using sample news api response

--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
-        
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={imageUrl}
+            className="card-img-top"
+            alt="..."
+            style={{ height: "12rem" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
