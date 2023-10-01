e82a3aecd8aba5602cc39107d731fdfbaac90fa6
shreyashkatkar07 <shreyashkatkar04@gmail.com>
2023-10-01 15:47:03 +0530

    added previous and next button

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,42 +2,99 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class News extends Component {
-
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
-  async componentDidMount(){
-
-    let url = "https://newsapi.org/v2/everything?q=apple&from=2023-09-29&to=2023-09-29&sortBy=popularity&apiKey=9dcd9f3301e846388b89193203711165";
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=9dcd9f3301e846388b89193203711165";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles : parsedData.articles});
-
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+  handlePrevClick = async () => {
+    if (this.state.page - 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9dcd9f3301e846388b89193203711165&page=${
+        this.state.page - 1
+      }&pageSize=21`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page - 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 21)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9dcd9f3301e846388b89193203711165&page=${
+        this.state.page + 1
+      }&pageSize=21`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
   render() {
     return (
       <>
         <div className="container my-3">
           <h3>News Hub - Top Headlines</h3>
-
           <div className="row">
             {this.state.articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <Newsitem
                     title={element.title ? element.title.slice(0, 30) : ""}
-                    description={element.description ? element.description.slice(0, 45) : ""}
-                    imageUrl={element.urlToImage ? element.urlToImage : "https://images.macrumors.com/t/on82wv5858RI4ckSlMLJ1hrpSPY=/2250x/article-new/2022/01/iOS-App-Store-General-Feature-Desaturated.jpg"}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 45)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://images.macrumors.com/t/on82wv5858RI4ckSlMLJ1hrpSPY=/2250x/article-new/2022/01/iOS-App-Store-General-Feature-Desaturated.jpg"
+                    }
                     newsUrl={element.url}
                   />
                 </div>
               );
             })}
           </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handlePrevClick}
+            disabled={this.state.page <= 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handleNextClick}
+            disabled={
+              this.state.page + 1 > Math.ceil(this.state.totalResults / 21)
+            }
+          >
+            Next &rarr;
+          </button>
         </div>
       </>
     );
